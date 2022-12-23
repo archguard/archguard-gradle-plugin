d80aa2839b0388f83936b61065a3649dedf5fba4
Phodal Huang <h@phodal.com>
2022-12-23 08:45:20 +0800

    feat: init report container

---
+++ plugin-build/plugin/src/main/kotlin/org/archguard/scanner/gradle/plugin/config/ArchGuardReportContainerImpl.kt
@@ -0,0 +1,28 @@
+package org.archguard.scanner.gradle.plugin.config
+
+import org.gradle.api.Task
+import org.gradle.api.internal.CollectionCallbackActionDecorator
+import org.gradle.api.reporting.ConfigurableReport
+import org.gradle.api.reporting.DirectoryReport
+import org.gradle.api.reporting.SingleFileReport
+import org.gradle.api.reporting.internal.TaskGeneratedSingleDirectoryReport
+import org.gradle.api.reporting.internal.TaskGeneratedSingleFileReport
+import org.gradle.api.reporting.internal.TaskReportContainer
+
+
+class ArchGuardReportContainerImpl : ArchGuardReportContainer, TaskReportContainer<ConfigurableReport> {
+    constructor(task: Task, decorator: CollectionCallbackActionDecorator) : super(
+        ConfigurableReport::class.java,
+        task,
+        decorator
+    ) {
+        add(TaskGeneratedSingleDirectoryReport::class.java, "html", task, "index.html")
+        add(TaskGeneratedSingleFileReport::class.java, "xml", task)
+        add(TaskGeneratedSingleFileReport::class.java, "csv", task)
+    }
+
+    override val html: DirectoryReport? get() = getByName("html") as DirectoryReport?
+    override val xml: SingleFileReport? get() = getByName("xml") as SingleFileReport?
+    override val csv: SingleFileReport? get() = getByName("csv") as SingleFileReport?
+
+}
--- plugin-build/plugin/src/main/kotlin/org/archguard/scanner/gradle/plugin/config/ReportContainer.kt
+++ plugin-build/plugin/src/main/kotlin/org/archguard/scanner/gradle/plugin/config/ReportContainer.kt
@@ -2,14 +2,17 @@ package org.archguard.scanner.gradle.plugin.config
 
 import org.gradle.api.reporting.ConfigurableReport
 import org.gradle.api.reporting.DirectoryReport
+import org.gradle.api.reporting.ReportContainer
 import org.gradle.api.reporting.SingleFileReport
-import org.gradle.api.reporting.internal.TaskReportContainer
 import org.gradle.api.tasks.Internal
 
-interface ArchGuardReportContainer: TaskReportContainer<ConfigurableReport> {
-    @Internal
-    fun getHtml(): DirectoryReport?
+interface ArchGuardReportContainer: ReportContainer<ConfigurableReport?> {
+    @get:Internal
+    val html: DirectoryReport?
 
-    @Internal
-    fun getXml(): SingleFileReport?
+    @get:Internal
+    val xml: SingleFileReport?
+
+    @get:Internal
+    val csv: SingleFileReport?
 }
